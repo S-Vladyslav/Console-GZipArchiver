d161ed5f594207e12e1f3db173f71fec62a24ac3
Владислав Валерійович <vladyslavsyrotyuk@gmail.com>
2022-02-13 02:21:22 +0200

    Add project files.

---
+++ GZipArchiver/Archiver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace GZipArchiver
+{
+    class Archiver
+    {
+        static int Compress(string sourceFile, string compressedFile)
+        {
+            using (FileStream fileReaderStream = new FileStream(sourceFile, FileMode.Open))
+            {
+                using (FileStream fileWriterStream = File.Create(compressedFile))
+                {
+                    using (GZipStream fileCompressorStream = new GZipStream(fileWriterStream, CompressionMode.Compress))
+                    {
+                        fileReaderStream.CopyTo(fileCompressorStream);
+                        Console.WriteLine($"{fileReaderStream.Length}\t{fileWriterStream.Length}");
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        static int Decompress(string compressedFile, string targetFile)
+        {
+            using (FileStream fileReaderStream = new FileStream(compressedFile, FileMode.Open))
+            {
+                using (FileStream fileWriterStream = File.Create(targetFile))
+                {
+                    using (GZipStream fileDecompressorStream = new GZipStream(fileReaderStream, CompressionMode.Decompress))
+                    {
+                        fileDecompressorStream.CopyTo(fileWriterStream);
+                        Console.WriteLine($"{fileReaderStream.Length}\t{fileWriterStream.Length}");
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ GZipArchiver/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace GZipArchiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || args[0] == "-h" || args[0] == "--help")
+            {
+                WriteHelp();
+                return;
+            }
+
+            Dictionary<string, string> argumentsDict = ParseArgs(args);
+
+
+        }
+
+        static Dictionary<string, string> ParseArgs(string[] args)
+        {
+            Dictionary<string, string> argsDict = new Dictionary<string, string>();
+
+            argsDict.Add("archiveName", args[0]);
+            argsDict.Add("action", args[1]);
+
+            string files = "";
+
+            for (int i = 2; i < args.Length; i++)
+            {
+                files += $" {args[i]}";
+            }
+
+            argsDict.Add("files", files);
+
+            return new Dictionary<string, string>();
+        }
+
+        static void WriteHelp()
+        {
+            Console.WriteLine("*help*");
+        }
+    }
+}
